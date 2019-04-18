3338052089f760efbd44a5c375d1732199ab6fd4
ROBERT Lowry <lowry7620@students.cptc.edu>
2019-04-18 15:16:12 -0700

    Basic prototype of student registration web page

---
+++ typescript/main.ts
@@ -0,0 +1,38 @@
+/**
+ * Represents a CPTC Student
+ */
+class Student{
+    firstName:string;
+    lastName:string;
+    dateOfBirth:Date;
+}
+
+//test code
+let testStu:Student = new Student();
+testStu.firstName = "Joe";
+testStu.lastName = "Ortiz";
+testStu.dateOfBirth = new Date(1980, 3, 1); //March 1st 1980
+
+window.onload = function(){
+    let regBtn = <HTMLElement> document.querySelector("main > button");
+    regBtn.onclick = registerStudent;
+}
+
+function registerStudent(){
+    let nextStudent:Student = getStudent();
+    displayStudent(nextStudent);
+}
+
+function getStudent():Student{
+    let s = new Student();
+    let fName = (<HTMLInputElement> document.getElementById("first-name")).value;
+    s.firstName = fName;
+    s.lastName = (<HTMLInputElement> document.getElementById("first-name")).value;
+    //TODO: get date of birth
+    return s;
+}
+
+function displayStudent(stu:Student){
+    alert(stu.firstName + " " + stu.lastName);
+
+}
